c826bff95ee3c9957f9193b8f4bfbf003a70fd78
jamison904 <james.jamison85@gmail.com>
2013-11-30 16:46:49 -0500

    Source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Nov 22 13:23:37 KST 2013"
+#define LINUX_COMPILE_BY "omsgsm"
+#define LINUX_COMPILE_HOST "sy918-desktop"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
